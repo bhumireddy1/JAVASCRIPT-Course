2197f6749423a402072de79397b8f4f2fc05cc93
bhumireddy1 <sunilkumareddy62@gmail.com>
2022-10-17 17:10:31 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// Declare a varaible and initialize it
+// Global varaible declaration
+var name = "sunilkumsr";
+
+// Function defination
+function myFunction() {
+     var num = 45;
+     console.log(name);
+     console.log("\n" + num);
+}
+myFunction();
